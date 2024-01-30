adf876ef13a65a73ebe0909d459d44e10777cded
Chamoli <godchamoli@gmail.com>
2024-01-30 19:08:22 +0530

    Completed frontend

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const Form = () => {
   const [response, setResponse] = useState(null);
+  const [flag, setFlag] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -9,7 +13,7 @@ const Form = () => {
     const formData = new FormData(event.target);
     
     try {
-      const response = await fetch('https://facedetectorbackend.onrender.com/', {
+      const response = await fetch(process.env.URL, {
         method: 'POST',
         body: formData
       });
@@ -18,9 +22,13 @@ const Form = () => {
       setResponse(data);
     } catch (error) {
         console.log("Not able to fetch the data");
-      console.error('Error:', error);
+        console.error('Error:', error);
     }
   };
+
+  const clearInformation = () => {
+    setFlag(!flag);
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
@@ -31,14 +39,17 @@ const Form = () => {
             <label htmlFor="image" className="block text-gray-700 font-bold mb-2">Image:</label>
             <input type="file" id="image" name="file" className="border-2 border-gray-300 rounded-lg p-2 w-full" />
           </div>
-          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={clearInformation}>
             Submit
           </button>
         </form>
-        {response && (
-          <div className="mt-4">
-            <h2>Response from Backend:</h2>
-            <pre>{JSON.stringify(response, null, 2)}</pre>
+        {flag && response && (
+          <div className="mt-4 bg-gray-100 rounded-lg p-4 shadow-inner">
+            <h2 className="text-lg font-semibold mb-2">Information:</h2>
+            <div>
+                <span className="text-gray-700">Face detected: {response.message}</span><br/>
+                <span className="text-gray-700">No of faces: {response.num_faces}</span>
+            </div>
           </div>
         )}
       </div>
